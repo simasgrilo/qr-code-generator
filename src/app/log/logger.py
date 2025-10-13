4edb20500ab3d6ec755c54591051beb2f2b986be
Erick <simasgrilo86@gmail.com>
2025-10-13 00:47:03 -0300

    Add backupCount for RotatingFileHandler as a env var, refactor of init logging messages

--- src/app/log/logger.py
+++ src/app/log/logger.py
@@ -10,6 +10,9 @@ class Logger:
     """
     instance = None
     DEFAULT_LOG_PATH = 'logs\\app.log'
+    LOG_FILE = 'LOG_FILE'
+    LOG_FILE_ROTATION = 'LOG_FILE_ROTATION'
+    LOG_FILE_BACKUP_COUNT = 'LOG_FILE_BACKUP_COUNT'
 
     def __new__(cls):
         if cls.instance is not None:
@@ -20,14 +23,21 @@ class Logger:
     def __init__(self):
         self.logger = logging.getLogger("qr_code_logger")
         self.logger.setLevel(logging.INFO)
-        log_directory = os.getenv("LOG_FILE")
-        rotation = os.getenv("LOG_FILE_ROTATION")
+        log_directory = os.getenv(self.LOG_FILE)
+        rotation = os.getenv(self.LOG_FILE_ROTATION)
         if not log_directory:
-            self.logger.info("missing .env var LOG_FILE. Defaulting to log path logs/app.log")
+            self.logger.info("missing .env var %s. Defaulting to log path logs/app.log", self.LOG_FILE)
             log_directory = os.path.join(Path(__file__).parent, self.DEFAULT_LOG_PATH)
         if not rotation:
-            self.logger.info("missing .env var LOG_FILE_ROTATION. Defaulting to log path logs/app.log")
+            self.logger.info("missing .env var %s. Defaulting to log path logs/app.log", self.LOG_FILE_ROTATION)
             rotation = 'midnight'
+        try:
+            backup_count = int(os.getenv(self.LOG_FILE_BACKUP_COUNT))
+            if not backup_count:
+                raise ValueError
+        except (ValueError, KeyError, TypeError):
+            self.logger.info('provided .env entry for %s is invalid. Defaulting to 7 days',self.LOG_FILE_BACKUP_COUNT)
+            backup_count = 7
         handler = TimedRotatingFileHandler(
             filename=log_directory,
             when=rotation,
