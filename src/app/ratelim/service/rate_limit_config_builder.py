0cb4447327c2d10d7e06a42989d1ae10e417447d
Erick <simasgrilo86@gmail.com>
2025-11-23 16:49:40 -0300

    Add: in-memory data store for rate limiting

--- src/app/ratelim/service/rate_limit_config_builder.py
+++ src/app/ratelim/service/rate_limit_config_builder.py
@@ -5,7 +5,7 @@ import dotenv
 from typing import Optional, Dict, Any
 from redis.exceptions import ConnectionError
 from src.app.ratelim.models.rate_limit_config import RateLimitConfig
-from src.app.ratelim.service.redis_manager import RedisStore, InMemoryStore
+from src.app.ratelim.service.data_store_manager import RedisStore, InMemoryStore
 from src.app.exceptions.data_store_conn_error import DataStoreConnectionError
 
 def get_rate_limiter_instance(test_config: Optional[Dict[str, Any]]):
@@ -21,9 +21,10 @@ def get_rate_limiter_instance(test_config: Optional[Dict[str, Any]]):
                                              relevant configuration
     """
     if test_config:
-        data_store = InMemoryStore()
+        in_memory_cap = int(test_config.get("in_memory_cap", 100))
         cooldown_time = int(test_config.get("cooldown_time", 30))
         num_requests = int(test_config.get("num_requests", 10))
+        data_store = InMemoryStore(in_memory_cap)
         activity = test_config.get("activity", "qr_code_gen")
     else:
         dotenv.load_dotenv()
@@ -34,7 +35,8 @@ def get_rate_limiter_instance(test_config: Optional[Dict[str, Any]]):
             data_store = RedisStore.create()
         except DataStoreConnectionError:
             # an issue with redis upon getting the instance
-            data_store = {}
+            in_memory_cap = int(os.getenv("RATE_LIMITER_IN_MEMORY_CAP"))
+            data_store = InMemoryStore(in_memory_cap)
             #data_store = MemoryRateLimiter(rate_limit_config)
     rate_limit_config = RateLimitConfig(
         data_store=data_store,
